ccbec275e7788ef112f4c67c86d283646897357c
AadarsanDahal <aadarsandahal@gmail.com>
2025-02-16 11:06:17 +0545

    ex 2

--- LAB_3/LAB3_ex2.c
+++ LAB_3/LAB3_ex2.c
@@ -5,16 +5,16 @@ int main(){
     int i,j;
 
 
-    for(i=1; i<=5 ; i++){
+    for(i=5; i>=1 ; i--){
          for (j=5 ; j>i ;j--){
-                printf(" ");
+                printf(" ");        // for printing spaces
             }
 
             for (j=1 ; j<=i ;j++){
-                printf("%d",j);
+                printf("%d",j);   // for printing rows    
             }
              for (j=i-1 ; j>=1 ;j--){
-                printf("%d",j);
+                printf("%d",j);         // for printing columns
             }
             printf("\n");
            
